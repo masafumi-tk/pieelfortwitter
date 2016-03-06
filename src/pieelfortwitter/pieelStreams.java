cbaedbe9e60501eadcbe6df297a4c66e3a648e8f
tk <masafumi.cascata@gmail.com>
2016-03-07 03:20:07 +0900

    20160307

--- src/pieelfortwitter/pieelStreams.java
+++ src/pieelfortwitter/pieelStreams.java
@@ -85,7 +85,7 @@ public class pieelStreams
     }
     
     
-    //“ú–{‚ªŠÜ‚Ü‚ê‚Ä‚¢‚é‚©‚Ì”»’f
+    //“ú–{‚ªŠÜ‚Ü‚ê‚Ä‚¢‚é‚©‚Ì”»’fhoge
     public static boolean isJap(String str)
     {
     	for(int i = 0 ; i < str.length() ; i++) {
